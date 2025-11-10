c6d4eeb5a31e6ec0d017c0ebcdc4a17cbe733b57
Del:Tango <alvearesolutions@gmail.com>
2025-11-10 16:39:02 -0500

    Updated version

--- setup.py
+++ setup.py
@@ -6,14 +6,11 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-#   with open("requirements.txt", "r", encoding="utf-8") as fh:
-#       requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
-
 setup(
     name="pyroform",
-    version="0.1.0",
-    author="Pyroform Team",
-    description="Linux Configurator tool written in Python3",
+    version="1.0.0",
+    author="Alveare Solutions",
+    description="Python3 Linux Configurator Tool",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
@@ -39,21 +36,7 @@ setup(
 
 # CODE DUMP
 
-#   # setup.py
-#   from setuptools import setup, find_packages
+#   with open("requirements.txt", "r", encoding="utf-8") as fh:
+#       requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
 
-#   setup(
-#       name="pyroform",
-#       version="0.1.0",
-#       packages=find_packages(),
-#       install_requires=[
-#           "flow-ctrl>=1.0.0",
-#           "PyYAML>=6.0",
-#           "click>=8.0.0"
-#       ],
-#       entry_points={
-#           'console_scripts': [
-#               'pyroform=pyroform.cli:main',
-#           ],
-#       }
-#   )
+
